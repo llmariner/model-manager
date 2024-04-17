2bf9271d07e99d1c3541d5332d072619957360d2
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-04-17 15:45:45 -0700

    Add hugging face downloader

---
+++ loader/internal/loader/hugging_face_downloader.go
@@ -0,0 +1,43 @@
+package loader
+
+import (
+	"bytes"
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+// NewHuggingFaceDownloader creates a new HuggingFaceDownloader.
+func NewHuggingFaceDownloader(cacheDir string) *HuggingFaceDownloader {
+	return &HuggingFaceDownloader{
+		cacheDir: cacheDir,
+	}
+}
+
+// HuggingFaceDownloader downloads models from Hugging Face.
+type HuggingFaceDownloader struct {
+	cacheDir string
+}
+
+func (h *HuggingFaceDownloader) download(modelName, destDir string) error {
+	// Download the image with huggingface-cli. If the access to the specified model is gated,
+	// the environment variable HUGGING_FACE_HUB_TOKEN needs to be set.
+	cmdline := []string{
+		"huggingface-cli",
+		"download",
+		modelName,
+		fmt.Sprintf("--local-dir=%s", destDir),
+		fmt.Sprintf("--cache-dir=%s", h.cacheDir),
+		"--local-dir-use-symlinks=True",
+		"--quiet",
+	}
+	cmd := exec.Command(cmdline[0], cmdline[1:]...)
+	var errb bytes.Buffer
+	cmd.Stderr = &errb
+	if err := cmd.Run(); err != nil {
+		log.Printf("Failed to download the image: %s", errb.String())
+		return fmt.Errorf("download: %s", err)
+	}
+
+	return nil
+}
