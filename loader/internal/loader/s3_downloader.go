685f68e5b15885eaf991880f59d78df8ea23ab98
Kenji Kaneda <kenji.kaneda@gmail.com>
2024-04-23 09:51:05 -0700

    Support download from S3

---
+++ loader/internal/loader/s3_downloader.go
@@ -0,0 +1,78 @@
+package loader
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"github.com/aws/aws-sdk-go/service/s3"
+)
+
+type s3Client interface {
+	Download(w io.WriterAt, key string) error
+	ListObjectsPages(prefix string, f func(page *s3.ListObjectsOutput, lastPage bool) bool) error
+}
+
+// NewS3Downloader returns a new S3Downloader.
+func NewS3Downloader(s3Client s3Client, pathPrefix string) *S3Downloader {
+	return &S3Downloader{
+		s3Client:   s3Client,
+		pathPrefix: pathPrefix,
+	}
+}
+
+// S3Downloader downloads models from S3.
+type S3Downloader struct {
+	s3Client   s3Client
+	pathPrefix string
+}
+
+func (d *S3Downloader) download(modelName, destDir string) error {
+	log.Printf("Downloading the model %q\n", modelName)
+
+	var keys []string
+	f := func(page *s3.ListObjectsOutput, lastPage bool) bool {
+		for _, obj := range page.Contents {
+			keys = append(keys, *obj.Key)
+		}
+		return lastPage
+	}
+	prefix := filepath.Join(d.pathPrefix, modelName)
+	if err := d.s3Client.ListObjectsPages(prefix, f); err != nil {
+		return err
+	}
+	if len(keys) == 0 {
+		return fmt.Errorf("no objects found under %s", prefix)
+	}
+
+	for _, key := range keys {
+		if err := d.downloadOneObject(key, prefix, destDir); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (d *S3Downloader) downloadOneObject(key, prefix, destDir string) error {
+	log.Printf("key    = %q\n", key)
+	log.Printf("prefix = %q\n", prefix)
+	filePath := filepath.Join(destDir, strings.TrimPrefix(key, prefix))
+	f, err := os.Create(filePath)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		_ = f.Close()
+	}()
+
+	log.Printf("Downloading S3 object %q and writing to %q\n", key, filePath)
+	if err := d.s3Client.Download(f, key); err != nil {
+		return err
+	}
+
+	return nil
+}
--- loader/internal/s3/client.go
+++ loader/internal/s3/client.go
@@ -4,26 +4,52 @@ import (
 	"io"
 
 	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/aws/aws-sdk-go/service/s3/s3manager"
-	"github.com/llm-operator/model-manager/loader/internal/config"
 )
 
+const (
+	partMiBs int64 = 128
+)
+
+// NewOptions is the options for NewClient.
+type NewOptions struct {
+	EndpointURL string
+	Region      string
+	Bucket      string
+
+	UseAnonymousCredentials bool
+}
+
 // NewClient returns a new S3 client.
-func NewClient(c config.S3Config) *Client {
-	sess := session.Must(session.NewSessionWithOptions(session.Options{
-		SharedConfigState: session.SharedConfigEnable,
-	}))
+func NewClient(o NewOptions) *Client {
+	var sopts session.Options
+	if o.UseAnonymousCredentials {
+		sopts = session.Options{
+			Config: aws.Config{
+				Credentials: credentials.AnonymousCredentials,
+			},
+		}
+	} else {
+		sopts = session.Options{
+			SharedConfigState: session.SharedConfigEnable,
+		}
+	}
+
+	sess := session.Must(session.NewSessionWithOptions(sopts))
+
 	conf := &aws.Config{
-		Endpoint: aws.String(c.EndpointURL),
-		Region:   aws.String(c.Region),
+		//		Credentials: sess.Config.Credentials,
+		Endpoint: aws.String(o.EndpointURL),
+		Region:   aws.String(o.Region),
 		// This is needed as the minio server does not support the virtual host style.
 		S3ForcePathStyle: aws.Bool(true),
 	}
 	return &Client{
 		svc:    s3.New(sess, conf),
-		bucket: c.Bucket,
+		bucket: o.Bucket,
 	}
 }
 
@@ -35,7 +61,6 @@ type Client struct {
 
 // Upload uploads the data that buf contains to a S3 object.
 func (c *Client) Upload(r io.Reader, key string) error {
-	const partMiBs int64 = 128
 	uploader := s3manager.NewUploaderWithClient(c.svc, func(u *s3manager.Uploader) {
 		u.PartSize = partMiBs * 1024 * 1024
 	})
@@ -48,4 +73,30 @@ func (c *Client) Upload(r io.Reader, key string) error {
 		return err
 	}
 	return nil
+}
+
+// Download downloads the data from a S3 object.
+func (c *Client) Download(w io.WriterAt, key string) error {
+	downloader := s3manager.NewDownloaderWithClient(c.svc, func(d *s3manager.Downloader) {
+		d.PartSize = partMiBs * 1024 * 1024
+	})
+	_, err := downloader.Download(w, &s3.GetObjectInput{
+		Bucket: aws.String(c.bucket),
+		Key:    aws.String(key),
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ListObjectsPages returns S3 objects with pagination.
+func (c *Client) ListObjectsPages(
+	prefix string,
+	f func(page *s3.ListObjectsOutput, lastPage bool) bool,
+) error {
+	return c.svc.ListObjectsPages(&s3.ListObjectsInput{
+		Bucket: aws.String(c.bucket),
+		Prefix: aws.String(prefix),
+	}, f)
 }
