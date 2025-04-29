c738f92b02ce812f7a3a2f246d468645e3e38907
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-04-29 14:32:44 -0700

    feat: support graceful shutdown

--- server/cmd/run.go
+++ server/cmd/run.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/go-logr/stdr"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
@@ -112,15 +116,30 @@ func run(ctx context.Context, c *config.Config) error {
 		errCh <- http.ListenAndServe(fmt.Sprintf(":%d", c.HTTPPort), mux)
 	}()
 
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+
+	s := server.New(st, logger)
 	go func() {
-		s := server.New(st, logger)
 		errCh <- s.Run(ctx, c.GRPCPort, c.AuthConfig, usageSetter)
 	}()
 
+	ws := server.NewWorkerServiceServer(st, logger)
 	go func() {
-		s := server.NewWorkerServiceServer(st, logger)
-		errCh <- s.Run(ctx, c.WorkerServiceGRPCPort, c.AuthConfig)
+		errCh <- ws.Run(ctx, c.WorkerServiceGRPCPort, c.AuthConfig)
 	}()
 
-	return <-errCh
+	select {
+	case err := <-errCh:
+		return err
+	case sig := <-sigCh:
+		log.Info("Got signal %v. Wait for %s before graceful shutdown", sig, c.GracefulShutdownDelay)
+		time.Sleep(c.GracefulShutdownDelay)
+
+		log.Info("Starting graceful shutdown.")
+		s.GracefulStop()
+		ws.GracefulStop()
+
+		return nil
+	}
 }
