d88d4a80ded736a36ee26f34d5f7e4c6dce9150f
Kenji Kaneda <kenji@kanedas-Mac-mini.local>
2025-07-25 07:58:35 -0700

    feat: support ProjectID in the HFModelRepo RPCs

--- server/internal/server/hf_model_repos_test.go
+++ server/internal/server/hf_model_repos_test.go
@@ -44,3 +44,40 @@ func TestHFModelRepo(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, got.Name, repoName)
 }
+
+func TestHFModelRepo_Project(t *testing.T) {
+	st, tearDown := store.NewTest(t)
+	defer tearDown()
+
+	const (
+		repoName  = "r0/m0"
+		projectID = "p0"
+	)
+
+	wsrv := NewWorkerServiceServer(st, testr.New(t))
+	ctx := fakeAuthInto(context.Background())
+	_, err := wsrv.GetHFModelRepo(ctx, &v1.GetHFModelRepoRequest{
+		Name:      repoName,
+		ProjectId: projectID,
+	})
+	assert.Error(t, err)
+	assert.Equal(t, codes.NotFound, status.Code(err))
+
+	_, err = wsrv.CreateHFModelRepo(ctx, &v1.CreateHFModelRepoRequest{
+		Name:      repoName,
+		ProjectId: projectID,
+	})
+	assert.NoError(t, err)
+
+	r, err := st.GetHFModelRepo(repoName, projectID, defaultTenantID)
+	assert.NoError(t, err)
+	assert.Equal(t, r.Name, repoName)
+	assert.Equal(t, r.ModelID, "r0-m0")
+
+	got, err := wsrv.GetHFModelRepo(ctx, &v1.GetHFModelRepoRequest{
+		Name:      repoName,
+		ProjectId: projectID,
+	})
+	assert.NoError(t, err)
+	assert.Equal(t, got.Name, repoName)
+}
