b7f92ce0d76cf85e686220fa1744c6d29d378303
Kenji Kaneda <kenji.kaneda@gmail.com>
2025-07-25 16:01:00 -0700

    Update server/internal/store/base_model_test.go

--- server/internal/store/base_model_test.go
+++ server/internal/store/base_model_test.go
@@ -63,7 +63,7 @@ func TestBaseModel(t *testing.T) {
 	assert.Equal(t, "path", gotMs[0].Path)
 	assert.Equal(t, "gguf_model_path", gotMs[0].GGUFModelPath)
 
-	c, err := st.CountBaseModels("", tenantID)
+	c, err := st.CountBaseModels(tenantID)
 	assert.NoError(t, err)
 	assert.Equal(t, int64(1), c)
 
